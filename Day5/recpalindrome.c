819356a7b5cd021ed847063113c6fef8af44338d
AshwinSubramanyaGS <ashwin.subramanya@gmail.com>
2024-11-09 13:25:53 +0530

    end of day 6

--- Day5/recpalindrome.c
+++ Day5/recpalindrome.c
@@ -4,12 +4,20 @@
 int isPallin(char*);
 int main()
 {
-    printf("Hello World");
+    char s[100];
+    scanf("%s",s);
+    printf("%s is %s",s,isPallin(s)?"a pallindrome":"not a pallindrome");
     return 0;
 }
-
 int isPallin(char *s )
 {
-    static int i=0,len=strlen(s);
-    if(s[i]!=s[len-i])return 0;
+    static int i=0;
+    int len=strlen(s);
+    if(i>=len/2)return 1;
+    if(s[i]!=s[len-1-i])return 0;
+    else
+    {
+        i++;
+        return isPallin(s);
+    }
 }
